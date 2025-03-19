f0d57c5a1d8d6a5c8393593b870fd2f4b56640c4
Mate Pušić <mate@bjelopic.com>
2025-03-19 20:37:36 +0100

    styling changes

--- page.php
+++ page.php
@@ -1,14 +1,8 @@
 <?php
 get_header();
 ?>
-<div class="bg-neutral-900 px-8 py-12">
-	<article id="page-<?php the_ID(); ?>" <?php post_class('max-w-screen-lg mx-auto'); ?>>
-
-
-		<header class="entry-header mb-4">
-			<?php the_title(sprintf('<h1 class="entry-title text-2xl lg:text-5xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h1>'); ?>
-			<time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
-		</header>
+<div class="">
+	<article id="page-<?php the_ID(); ?>" <?php post_class('mx-auto'); ?>>
 
 		<div class="entry-content">
 			<?php if (has_post_thumbnail()) { ?>
